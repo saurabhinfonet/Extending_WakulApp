d927844087e9da8beee81da1149761a9d57d1de9
Saurabh Sharma <saurabhinfonet@gmail.com>
2017-06-07 08:07:52 +1000

    Hypervisor

---
+++ Hypervisor/README.md
@@ -0,0 +1,20 @@
+This module is for creating VM and running harvester software in new VM automatically and also for terminating VMs.
+
+Steps to create VM , download necessary packages and run harvester program :
+
+1. Insert configuration details into files/configure.yaml
+
+2. Run create_vm.py
+
+   ```bash
+   python3 create_vm.py
+   ```
+
+Steps to terminate VM on fcfs mode :
+
+1. Insert configuration details into files/configure.yaml
+
+2. Run terminate_vm.py
+
+   ```bash
+   python3 terminate_vm.py
---
+++ Hypervisor/download_packages.sh
@@ -0,0 +1,18 @@
+#Created by COMP90024 Assignment 2 CCC Team 8, Semester 1 2017
+#!/bin/bash
+# setup environment
+sudo apt-get update
+
+# git setup
+sudo apt-get -y install git
+
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python-nltk
+sudo apt-get -y install python-dev python-setuptools python-numpy python-scipy libatlas-dev
+#extras for python3
+sudo apt-get -y install python3-dev python3-setuptools python3-numpy python3-scipy
+
+sudo apt-get -y install python-matplotlib
+sudo apt-get -y install python3-matplotlib
+
+sudo pip3 install -r requirements.txt
